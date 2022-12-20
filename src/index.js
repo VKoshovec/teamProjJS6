602682b61849c53490e32560d7c8669d98e61e0b
VladimirKoshovec <koshavladmir@gmail.com>
2022-12-20 16:20:36 +0200

    upd_linkapi

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import * as part2 from './js/part2';
 import { movieLink } from './js/linkapi';
 
 movieLink.getMovies(1).then (res => console.log(res));
+movieLink.getMoviesByWord('Аватар').then (res => console.log(res));
+movieLink.getMoviesById(19995).then (res => console.log(res));
--- src/js/linkapi.js
+++ src/js/linkapi.js
@@ -3,12 +3,20 @@ import Notiflix from "notiflix";
 
 const keyApi = '0bf9a11da9d083f4751315d07dcbd89b';
 const baseUrl = 'https://api.themoviedb.org/3/trending/movie/day';
+const searchByWodrUrl = 'https://api.themoviedb.org/3/search/movie';
+const searchByIdUrl = 'https://api.themoviedb.org/3/movie/';
 
 // Api test reqvest
 // https://api.themoviedb.org/3/movie/550?api_key=0bf9a11da9d083f4751315d07dcbd89b
 
 //Api popular movie in a day
 // https://api.themoviedb.org/3/trending/movie/day?api_key=0bf9a11da9d083f4751315d07dcbd89b
+
+//Api search by word
+// https://api.themoviedb.org/3/search/movie?api_key=0bf9a11da9d083f4751315d07dcbd89b&language=en-US&query=аватар&page=&include_adult=false
+
+//Api get movie about by id
+// https://api.themoviedb.org/3/movie/19995?language=en-US&api_key=0bf9a11da9d083f4751315d07dcbd89b
 
 export const movieLink = {
      
@@ -21,9 +29,45 @@ export const movieLink = {
         };
 
         try {
-           return  await axios.get ( baseUrl, { params } );
+           return await axios.get ( baseUrl, { params } );
         } catch (error) {
            Notiflix.Notify.failure('Some broblems with api or query!') 
+        };
+    },
+
+    //Objec method to get movies by name
+    async getMoviesByWord ( queryWord ) {
+            
+        const params = {
+            api_key: keyApi,
+            language: 'en-US',
+            query: queryWord,
+            include_adult: false,
+        };
+
+        try {
+
+           return await axios.get ( searchByWodrUrl, { params } );
+           
+        } catch (error) {
+           Notiflix.Notify.warning('There are no results!') 
+        };
+    },
+
+    //Objec method to get movie about by id
+    async getMoviesById ( movieId ) {
+            
+        const params = {
+            language: 'en-US',
+            api_key: keyApi,
+        };
+
+        try {
+
+           return await axios.get ( `${searchByIdUrl}${movieId}`, { params } );
+
+        } catch (error) {
+            Notiflix.Notify.failure('Some broblems with api or query!');
         };
     },
 
