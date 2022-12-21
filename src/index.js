327c7d18a5d106e3bd150ace18b3f7b972ed6940
Oleg Gubskiy <gubskyolegod@gmail.com>
2022-12-21 11:07:29 +0200

    Делает backdrop modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import * as part1 from './js/part';
 import * as part2 from './js/part2';
 import { movieLink } from './js/linkapi';
+import './js/modal/modal-main';
 
-movieLink.getMovies(1).then (res => console.log(res));
-movieLink.getMoviesByWord('Аватар').then (res => console.log(res));
-movieLink.getMoviesById(19995).then (res => console.log(res));
+movieLink.getMovies(1).then(res => console.log(res));
+movieLink.getMoviesByWord('Аватар').then(res => console.log(res));
+movieLink.getMoviesById(19995).then(res => console.log(res));
 console.log(movieLink.getImageUrl('198vrF8k7mfQ4FjDJsBmdQcaiyq.jpg', 500));
